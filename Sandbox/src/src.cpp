df537ac88c0aedb2f5156378f7561c03b02c0e80
jumailj <jumailj.work@gmail.com>
2023-03-18 12:29:33 +0530

    added graphic glfw submodule

--- Sandbox/src/src.cpp
+++ Sandbox/src/src.cpp
@@ -1,11 +1,16 @@
 #include "Flare2D.h"
 
+
+void glfw_init();
+
 int main(int argc, char* argv[]) {
 
 	Flare2D::Log::Init();
 	int a = 20;
 
 	FLARE2D_ERROR("I'M YOUR ERROR {0}",a);
+
+	glfw_init();
 
 
 }
